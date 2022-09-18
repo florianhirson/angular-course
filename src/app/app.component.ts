ca783c3c03a30f8b52393d31f71348448e9193e7
florianhirson <flohirson@gmail.com>
2022-09-18 15:18:28 +0200

    Learned the basics of angular built in pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,16 @@ export class AppComponent {
 
   courses = COURSES;
 
+  startDate = new Date(2000, 0, 1);
+
+  title  = COURSES[0].description;
+
+  price = 9.99556565656;
+
+  rate = 0.67;
+
+  course = COURSES[0];
+
   onCourseSelected(course:Course) {
     console.log("App component - click event bubbled...", course);
   }
