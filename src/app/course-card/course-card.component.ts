31f46cd571ec480d35ae97de9652414f06798ce4
florianhirson <flohirson@gmail.com>
2022-09-18 14:56:04 +0200

    Learned the basics of ngStyle

--- src/app/course-card/course-card.component.ts
+++ src/app/course-card/course-card.component.ts
@@ -38,4 +38,10 @@ export class CourseCardComponent implements OnInit {
       return 'beginner';
     }
   }
+
+  cardStyles() {
+    return {
+      'background-image': 'url(' + this.course.iconUrl + ')'
+    };
+  }
 }
