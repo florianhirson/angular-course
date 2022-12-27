52dd9373b08c2f3481661715ff6202e7d6edd32e
florianhirson <flohirson@gmail.com>
2022-12-27 17:31:43 +0100

    Learns the basics of manual injectable dependencies

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {AfterViewInit, Component, ElementRef, OnInit, QueryList, ViewChild, ViewChildren} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, Inject, InjectionToken, OnInit, QueryList, ViewChild, ViewChildren} from '@angular/core';
 import {COURSES} from '../db-data';
 import {Course} from './model/course';
 import {CourseCardComponent} from './course-card/course-card.component';
@@ -7,16 +7,31 @@ import {Observable} from 'rxjs';
 import {HttpClient, HttpParams} from '@angular/common/http';
 import {CoursesService} from './services/courses.service';
 
+//Function used to create an instance of the dependency
+function coursesServiceProvider(http:HttpClient): CoursesService {
+  return new CoursesService(http);
+}
+
+//Used to identify the dependency
+export const COURSES_SERVICE = new InjectionToken<CoursesService>('COURSES_SERVICE');
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [ //define the provider for the dependency
+    {
+      provide: COURSES_SERVICE, //injection token
+      useFactory: coursesServiceProvider, //factory function
+      deps: [HttpClient] //parameters of the factory function
+    }
+  ]
 })
 export class AppComponent implements OnInit {
 
   courses$ : Observable<Course[]>;
 
-  constructor(private  coursesService: CoursesService) {
+  constructor(@Inject(COURSES_SERVICE) private  coursesService: CoursesService) {
 
   }
 
